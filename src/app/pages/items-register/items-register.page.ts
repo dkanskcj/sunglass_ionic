b9af26cc01b9d869c911519f6f00a0bb12411c46
dkanskcj <dkanskcj@likelion.org>
2022-08-31 18:14:23 +0900

    db add

--- src/app/pages/items-register/items-register.page.ts
+++ src/app/pages/items-register/items-register.page.ts
@@ -1,5 +1,6 @@
-import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
+import { Component, ElementRef, EventEmitter, OnInit, Output, Renderer2, ViewChild } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { postcode } from 'src/assets/js/postcode.js';
 
 type Option = {
   value: string;
@@ -31,13 +32,14 @@ type segmentBtn = {
 })
 export class ItemsRegisterPage implements OnInit {
   @ViewChild('fileUploader') fileUploader: ElementRef;
+  @ViewChild('daum_popup', { read: ElementRef, static: true }) popup: ElementRef;
 
 
   display: FormControl = new FormControl("", Validators.required);
   file_store: FileList;
   file_list: Array<string> = [];
   urls = [];
-
+  frm: FormGroup;
   option_number: any;
 
   category: string = 'multiple';
@@ -111,7 +113,7 @@ export class ItemsRegisterPage implements OnInit {
       value: 'Setting'
     }
   ]
-  optionytypeList: segmentBtn[] =[
+  optionytypeList: segmentBtn[] = [
     {
       name: '단독형',
       value: 'single'
@@ -392,8 +394,8 @@ export class ItemsRegisterPage implements OnInit {
   handleSettings(settings: string) {
     this.settings = settings
   }
-  
-  handleOptionType(optiontype: string){
+
+  handleOptionType(optiontype: string) {
     this.optiontype = optiontype;
   }
 
@@ -496,12 +498,63 @@ export class ItemsRegisterPage implements OnInit {
     console.log(this.Options[1]);
   }
 
-  constructor() { }
+  constructor(
+    private formbuilder: FormBuilder,
+    private renderer: Renderer2
+  ) { }
 
   ngOnInit() {
+    this.frm = this.formbuilder.group({
+      addr1: [''],
+      addr2: [''],
+    })
   }
-
-
+  openDaumPopup() {
+    postcode(this.renderer, this.popup.nativeElement, data => {
+      this.frm.controls.addr1.setValue(data.address);
+      console.log(data);
+      /*
+          {
+              address: "대전 서구 둔산로 100"
+              addressEnglish: "100, Dunsan-ro, Seo-gu, Daejeon, Korea"
+              addressType: "R"
+              apartment: "N"
+              autoJibunAddress: ""
+              autoJibunAddressEnglish: ""
+              autoRoadAddress: ""
+              autoRoadAddressEnglish: ""
+              bcode: "3017011200"
+              bname: "둔산동"
+              bname1: ""
+              bname2: "둔산동"
+              buildingCode: "3017011200114200000021946"
+              buildingName: "대전광역시청"
+              hname: ""
+              jibunAddress: "대전 서구 둔산동 1420"
+              jibunAddressEnglish: "1420, Dunsan-dong, Seo-gu, Daejeon, Korea"
+              noSelected: "N"
+              postcode: "302-789"
+              postcode1: "302"
+              postcode2: "789"
+              postcodeSeq: "007"
+              query: "대전시청"
+              roadAddress: "대전 서구 둔산로 100"
+              roadAddressEnglish: "100, Dunsan-ro, Seo-gu, Daejeon, Korea"
+              roadname: "둔산로"
+              roadnameCode: "3166019"
+              sido: "대전"
+              sigungu: "서구"
+              sigunguCode: "30170"
+              userLanguageType: "K"
+              userSelectedType: "R"
+              zonecode: "35242"
+          }
+      */
+    });
+  }
+  closeDaumPopup() {
+    this.renderer.setStyle(this.popup.nativeElement, 'display', 'none');
+  }
 
 
 
